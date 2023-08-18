0ad3b7600c349fdf3afe31088df45435dbc93aa7
prechy123 <bamideleprecious100@gmail.com>
2023-08-18 12:35:36 +0100

    Switched from md5 to bcypt for better security

--- backend/controllers/userController.mjs
+++ backend/controllers/userController.mjs
@@ -1,32 +1,38 @@
 import User from "../model/userModel.mjs";
-import md5 from "md5";
+import bcrypt from "bcrypt"
 
-export const signupAccount = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (user) {
-      res.status(409).json({ message: "Account already exists" });
-    } else {
-      const encyptedPassword = md5(password);
-      await User.create({ email, password: encyptedPassword });
-      res.status(200).json({ message: "Account created successfully" });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-export const loginAccount = async (req, res) => {
-  const { email, password } = req.body;
-  const encyptedPassword = md5(password)
-  try {
-    const user = await User.findOne({ email, password: encyptedPassword });
-    if (user) {
-      res.status(200).json({ message: "Successfully logged in" });
-    } else {
-      res.status(400).json({ message: "Account not found." });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+// export const signupAccount = async (req, res) => {
+//   const { email, password } = req.body;
+//   const response = await User.findOne({email})
+//   if(response){
+//     const newUser = new User({
+//       email,
+//       password: md5(password),
+//     });
+//     newUser
+//       .save()
+//       .then(() => {
+//         res.status(200).json({ message: "account successfully created!!!" });
+//       })
+//       .catch((error) => {
+//         res.status(500).json({ error: error.message });
+//       });
+//   } else {
+//     res.status(200).json({message: "Email address exist!!!"})
+//   }
+// };
+
+// export const loginAccount = (req, res) => {
+//   const { email, password } = req.body;
+//   User.findOne({ email })
+//     .then((doc) => {
+//       if (doc.password === md5(password)) {
+//         res.status(200).json({ message: "account sucessfully logged in" });
+//       } else {
+//         res.status(400).json({ message: "account not found hahahahh" });
+//       }
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: error.message });
+//     });
+// };
