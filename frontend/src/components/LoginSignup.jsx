2b7d85cca525bc9f0ec579cd086a244cb8295335
prechy123 <bamideleprecious100@gmail.com>
2023-08-22 14:10:24 +0100

    Added loading state to loginSigup component

--- frontend/src/components/LoginSignup.jsx
+++ frontend/src/components/LoginSignup.jsx
@@ -10,81 +10,92 @@ export default function LoginSignup({
   const [signupPassword, setSignupPassword] = useState("");
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [loadingState, setLoadingState] = useState(false);
   const handleSignupSubmit = (e) => {
     e.preventDefault();
+    setLoadingState(true);
     createAccount(signupEmail, signupPassword);
     setSignupEmail("");
     setSignupPassword("");
   };
   const handleLoginSubmit = (e) => {
     e.preventDefault();
-    loginAccount(loginEmail, loginPassword);
+    setLoadingState(true);
+    setTimeout(() => {
+      loginAccount(loginEmail, loginPassword);
+    }, 1000);
     setLoginEmail("");
     setLoginPassword("");
   };
   return (
     <>
-      {signup && (
-        <div className="signup">
-          <h2>Create a new account</h2>
-          <form method="post" onSubmit={handleSignupSubmit}>
-            <div className="inputbox">
-              <input
-                id="email"
-                type="email"
-                name="email"
-                value={signupEmail}
-                onChange={(e) => setSignupEmail(e.target.value)}
-                required
-              />
-              <span>Email Address</span>
+      {loadingState ? (
+        "loading..."
+      ) : (
+        <>
+          {signup && (
+            <div className="signup">
+              <h2>Create a new account</h2>
+              <form method="post" onSubmit={handleSignupSubmit}>
+                <div className="inputbox">
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={signupEmail}
+                    onChange={(e) => setSignupEmail(e.target.value)}
+                    required
+                  />
+                  <span>Email Address</span>
+                </div>
+                <div className="inputbox">
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    value={signupPassword}
+                    onChange={(e) => setSignupPassword(e.target.value)}
+                    minLength={10}
+                    required
+                  />
+                  <span>Password</span>
+                </div>
+                <input type="submit" value="Signup" id="submit" />
+              </form>
             </div>
-            <div className="inputbox">
-              <input
-                id="password"
-                type="password"
-                name="password"
-                value={signupPassword}
-                onChange={(e) => setSignupPassword(e.target.value)}
-                minLength={10}
-                required
-              />
-              <span>Password</span>
+          )}
+          {login && (
+            <div className="login">
+              <h2>Log in to account</h2>
+              <form method="post" onSubmit={handleLoginSubmit}>
+                <div className="inputbox">
+                  <input
+                    id="email"
+                    type="email"
+                    name="username"
+                    onChange={(e) => setLoginEmail(e.target.value)}
+                    value={loginEmail}
+                    required
+                  />
+                  <span>Email Address</span>
+                </div>
+                <div className="inputbox">
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                    value={loginPassword}
+                    minLength={10}
+                    required
+                  />
+                  <span>Password</span>
+                </div>
+                <input type="submit" value="Login" id="submit" />
+              </form>
             </div>
-            <input type="submit" value="Signup" id="submit" />
-          </form>
-        </div>
-      )}
-      {login && (
-        <div className="login">
-          <h2>Log in to account</h2>
-          <form method="post" onSubmit={handleLoginSubmit}>
-            <div className="inputbox">
-              <input
-                id="email"
-                type="email"
-                name="username"
-                onChange={(e) => setLoginEmail(e.target.value)}
-                value={loginEmail}
-                required
-              />
-              <span>Email Address</span>
-            </div>
-            <div className="inputbox">
-              <input
-                id="password"
-                type="password"
-                name="password"
-                onChange={(e) => setLoginPassword(e.target.value)}
-                value={loginPassword}
-                minLength={10}
-                required
-              />
-              <span>Password</span>
-            </div>
-            <input type="submit" value="Login" id="submit" />
-          </form>
-        </div>
+          )}
+        </>
       )}
     </>
   );
