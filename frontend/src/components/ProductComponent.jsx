3395c2a7e25ee7ed026567631fa130428a0603c9
prechy123 <bamideleprecious100@gmail.com>
2023-08-24 10:02:24 +0100

    Handle success when a food is saved sucessfully to the data base

--- frontend/src/components/ProductComponent.jsx
+++ frontend/src/components/ProductComponent.jsx
@@ -3,15 +3,16 @@ import Cookies from "js-cookie";
 import axios from "axios";
 
 export default function ProductComponent({ meal }) {
+  const [success, setSuccess] = useState(false)
   const [error, setError] = useState(false);
   const handleAddClick = async () => {
     const token = Cookies.get("token");
     try {
-      const response = await axios.post("http://127.0.0.1:4000/add", {
+      await axios.post("http://127.0.0.1:4000/add", {
         token: token,
         mealId: meal.idMeal,
       });
-      console.log(response.data);
+      setSuccess(true)
     } catch (err) {
       setError(true);
     }
@@ -28,6 +29,11 @@ export default function ProductComponent({ meal }) {
         <button onClick={handleAddClick} className="save-food">
           <span></span> Save Food
         </button>
+        {success && (
+          <h1 className="success">
+            Added Successfully 👍
+          </h1>
+        )}
         {error && (
           <h1 className="error">
             Login/signup to Account <a href="/account">Click here</a>
