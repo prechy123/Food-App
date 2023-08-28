f067109f95bb8aba9b30586a03a1984791b1a9af
prechy123 <bamideleprecious100@gmail.com>
2023-08-28 11:15:04 +0100

    Added animation to footer img

--- frontend/src/layouts/FooterLayout.jsx
+++ frontend/src/layouts/FooterLayout.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import facebook from '../image/facebook.png'
-import github from '../image/github.png'
-import instagram from '../image/instagram.png'
-import linkedin from '../image/linkedin.png'
+import React from "react";
+import { motion } from "framer-motion";
+import facebook from "../image/facebook.png";
+import github from "../image/github.png";
+import instagram from "../image/instagram.png";
+import linkedin from "../image/linkedin.png";
+
+const imgVariants = {
+  initial: {
+    x: "-100",
+  },
+  animate: {
+    x: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.5,
+    },
+  },
+};
 
 export default function FooterLayout() {
-    const date = new Date().getFullYear()
+  const date = new Date().getFullYear();
   return (
     <footer>
-        <div className='img'>
-            <img src={facebook} alt="facebook" width={30}/>
-            <img src={github} alt="github" width={30}/>
-            <img src={instagram} alt="instagram" width={30}/>
-            <img src={linkedin} alt="linkedin" width={30}/>
-        </div>
-        <div>
-            <p>Copyright {date}.</p>
-        </div>
+      <motion.div className="img" variants={imgVariants} initial="initial" animate="animate">
+        <motion.img variants={imgVariants} src={facebook} alt="facebook" width={30} />
+        <motion.img variants={imgVariants} src={github} alt="github" width={30} />
+        <motion.img variants={imgVariants} src={instagram} alt="instagram" width={30} />
+        <motion.img variants={imgVariants} src={linkedin} alt="linkedin" width={30} />
+      </motion.div>
+      <div>
+        <p>Copyright {date}.</p>
+      </div>
     </footer>
-  )
+  );
 }
