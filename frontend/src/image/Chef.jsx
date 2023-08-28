0556eeb8e60e13ede5c55ab176c5db190a036092
prechy123 <bamideleprecious100@gmail.com>
2023-08-28 10:46:58 +0100

    Styled logo svg

--- frontend/src/image/Chef.jsx
+++ frontend/src/image/Chef.jsx
@@ -4,66 +4,72 @@ import { motion } from "framer-motion";
 const svgVariants = {
   initial: {
     opacity: 0,
-	rotate: -180
+    rotate: -360,
   },
   animate: {
     opacity: 1,
-	rotate: 0,
-	transition: {
-		duration: 3,
-		ease: "easeOut"
-	}
+    rotate: 0,
+    transition: {
+      duration: 2,
+      ease: "easeOut",
+    },
   },
 };
 const pathVariants = {
   initial: {
     opacity: 0,
     pathLength: 0,
+    fill: "none",
   },
   animate: {
     opacity: 1,
     pathLength: 1,
+    fill: "rgb(0, 0, 0)",
     transition: {
       duration: 2,
-	  ease: "easeInOut"
+      ease: "easeInOut",
     },
   },
 };
-const pathVariants2 = {
-	initial: {
-	  opacity: 0,
-	  pathLength: 0,
-	},
-	animate: {
-	  opacity: 1,
-	  pathLength: 1,
-	  transition: {
-		duration: 2,
-		ease: "easeInOut",
-	  },
-	},
-  };
+const path2Variants = {
+  initial: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "none",
+  },
+  animate: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgb(0, 0, 0)",
+    transition: {
+      delay: 2,
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
 
 export default function Chef() {
   return (
     <>
       <motion.svg
-        version="1.0"
+        // version="1.0"
         xmlns="http://www.w3.org/2000/svg"
-        width="50.000000px"
-        height="50.000000px"
         viewBox="0 0 50.000000 50.000000"
-        preserveAspectRatio="xMidYMid meet"
+        // preserveAspectRatio="xMidYMid meet"
         variants={svgVariants}
         initial="initial"
         animate="animate"
+        className="icon-logo"
       >
         <g
           transform="translate(0.000000,50.000000) scale(0.009766,-0.009766)"
-          fill="#000000"
-          stroke="none"
+          // fill="#000000"
+          // stroke="#fff"
         >
-          <motion.path variants={pathVariants} initial="initial" animate="animate"
+          <motion.path
+            variants={pathVariants} fill="none"
             d="M2390 5104 c-256 -56 -463 -195 -604 -407 l-55 -83 -83 10 c-115 13
 -271 -5 -392 -45 -434 -144 -706 -567 -656 -1021 26 -233 118 -419 290 -588
 146 -144 323 -230 543 -265 l37 -6 0 -427 c0 -427 0 -427 24 -477 25 -54 71
@@ -88,7 +94,8 @@ export default function Chef() {
 -18 -73 c-25 -108 -34 -219 -19 -254 22 -54 105 -64 135 -17 7 11 18 67 24
 124 30 280 188 511 437 635 147 73 216 88 389 83 113 -3 142 -7 217 -33z"
           />
-          <motion.path variants={pathVariants2}
+          <motion.path
+            variants={path2Variants} fill="none"
             d="M966 1869 c-35 -27 -36 -86 -3 -116 12 -12 153 -96 312 -188 160 -92
 300 -176 313 -188 34 -31 28 -70 -25 -164 -26 -45 -50 -83 -53 -83 -4 0 -146
 80 -316 178 -368 213 -366 212 -399 212 -59 0 -97 -83 -57 -128 9 -11 168
