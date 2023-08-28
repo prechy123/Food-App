2967d9c7d76268008a1f83c6385cc9b628ae1bbc
prechy123 <bamideleprecious100@gmail.com>
2023-08-28 12:10:12 +0100

    Added AnimatePresence and useLocation to perform exit animation

--- frontend/src/AnimatedRoutes.js
+++ frontend/src/AnimatedRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
@@ -8,17 +8,21 @@ import ContactPage from "./pages/ContactPage";
 import ProductPage from "./pages/ProductPage";
 import AccountPage from "./pages/AccountPage";
 import CategoryContentPage from "./pages/CategoryContentPage";
+import { AnimatePresence } from "framer-motion";
 
 export default function AnimatedRoutes() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="search" element={<SearchPage />} />
-      <Route path="categories" element={<CategoriesPage />} />
-      <Route path="product/:id" element={<ProductPage />} />
-      <Route path="contact" element={<ContactPage />} />
-      <Route path="account" element={<AccountPage />} />
-      <Route path="categories/:cateName" element={<CategoryContentPage />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes location={location} key={location.key}>
+        <Route index element={<HomePage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="categories" element={<CategoriesPage />} />
+        <Route path="product/:id" element={<ProductPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="account" element={<AccountPage />} />
+        <Route path="categories/:cateName" element={<CategoryContentPage />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import HeaderLayout from "./layouts/HeaderLayout";
 import AnimatedRoutes from "./AnimatedRoutes";
 
-//pages
-// import HomePage from "./pages/HomePage";
-// import SearchPage from "./pages/SearchPage";
-// import CategoriesPage from "./pages/CategoriesPage";
-// import ContactPage from "./pages/ContactPage";
-// import ProductPage from "./pages/ProductPage";
-// import AccountPage from "./pages/AccountPage";
-// import CategoryContentPage from "./pages/CategoryContentPage";
 
 function App() {
   return (
