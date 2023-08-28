56a66e39dff484fc77a70713077a4b2f9306b91e
prechy123 <bamideleprecious100@gmail.com>
2023-08-28 11:59:36 +0100

    Added rolling animation to footer image

--- frontend/src/layouts/FooterLayout.jsx
+++ frontend/src/layouts/FooterLayout.jsx
@@ -5,7 +5,7 @@ import github from "../image/github.png";
 import instagram from "../image/instagram.png";
 import linkedin from "../image/linkedin.png";
 
-const imgVariants = {
+const divVariants = {
   initial: {
     x: "-100",
     opacity: 0
@@ -19,12 +19,28 @@ const imgVariants = {
     },
   },
 };
+const imgVariants = {
+  initial: {
+    x: "-100",
+    opacity: 0,
+    rotate: -360
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.5,
+    },
+  },
+};
 
 export default function FooterLayout() {
   const date = new Date().getFullYear();
   return (
     <footer>
-      <motion.div className="img" variants={imgVariants} initial="initial" animate="animate">
+      <motion.div className="img" variants={divVariants} initial="initial" animate="animate">
         <motion.img variants={imgVariants} src={facebook} alt="facebook" width={30} />
         <motion.img variants={imgVariants} src={github} alt="github" width={30} />
         <motion.img variants={imgVariants} src={instagram} alt="instagram" width={30} />
