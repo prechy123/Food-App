00092f365671a6e05411e666bacea2333ca1dd67
prechy123 <bamideleprecious100@gmail.com>
2023-08-28 11:00:50 +0100

    added animations to li in nav bar

--- frontend/src/layouts/HeaderLayout.jsx
+++ frontend/src/layouts/HeaderLayout.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-// import chef from "../image/chef.png";
 import { NavLink } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Chef from "../image/Chef";
+import { motion } from "framer-motion";
+
+
+const navVariants = {
+  initial: {
+    x: 100
+  },
+  animate: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 50
+    }
+  }
+}
 
 export default function HeaderLayout() {
   return (
@@ -11,14 +25,14 @@ export default function HeaderLayout() {
         <Chef />
         {/* <img src={chef} alt="chef" width={50} /> */}
         <ul>
-          <li>
+          <motion.li variants={navVariants} initial="initial" animate="animate">
             <NavLink to="/">HOME</NavLink>
             <NavLink to="/search">Search Food</NavLink>
             <NavLink to="/categories">Categories</NavLink>
             {/* <NavLink to="/product">Product</NavLink> */}
             <NavLink to="/contact">Contact us</NavLink>
             <NavLink to="/account">Account</NavLink>
-          </li>
+          </motion.li>
         </ul>
         <div className="menu">
           <span></span>
