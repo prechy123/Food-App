f9ee1485e61d499ea2556ad6c3c61c2656d36bc4
prechy123 <bamideleprecious100@gmail.com>
2023-09-03 14:53:51 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -53,7 +53,7 @@ export default function HomePage() {
           <div className="grid-container">
             <div className="grid-item-1"></div>
             <div className="grid-item-2"></div>
-            <div className="grid-item-3"></div>
+            <div className="grid-item-3"> </div>
             <div className="grid-item-4"></div>
             <div className="grid-item-5"></div>
             <div className="grid-item-6"></div>
